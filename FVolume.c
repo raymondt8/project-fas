f6ea025e765af529f9d8a066bffdd13ed1a65f99
raymondt8 <raymond.toft@gmail.com>
2016-11-10 10:18:26 +0100

    First outline of uniform grid generator

--- FVolume.c
+++ FVolume.c
@@ -1,6 +1,9 @@
 #include <stdlib.h>
 #include <stdio.h>
-
+typedef struct{
+    float density;
+    float
+}phase_t;
 
 typedef struct{
     float permeability;
@@ -11,14 +14,24 @@ typedef struct{
     float arealXDir;
     float arealYDir;
 }gridCell_t;
- 
+
 typedef struct{
     float xSize;
     float ySize;
-    float *mesh;
+    gridCell_t *mesh;
     float hight;
     float width;
 }grid_t;
+typedef grid_t *Grid;
+
+Grid generateUniformGrid(int xSize, int ySize){
+    Grid result = (Grid)malloc(sizeof(grid_t));
+    result->xSize = xSize;
+    result->ySize = ySize;
+    result->mesh = (gridCell_t)malloc(sizeof(gridCell_t)*xSize*ySize);
+    result->hight = 1.0/ySize;
+    result->width = 1.0/xsize;
+}
 
 float cellPermeability{
 
@@ -30,4 +43,8 @@ float transmissibility{
 
 float phaseMobility{
 
+}
+
+void finiteVolume(Grid grid){
+    float flow
 }
